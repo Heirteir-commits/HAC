586dcd342c9fb59ef22c9ed2470402748cea9131
heretere <heirteir@protonmail.com>
2020-12-02 05:07:34 -0500

    - Improved test class running

--- hac-util/src/test/java/com/heirteir/hac/util/dependency/plugin/DependencyPluginTest.java
+++ hac-util/src/test/java/com/heirteir/hac/util/dependency/plugin/DependencyPluginTest.java
@@ -1,9 +1,14 @@
 package com.heirteir.hac.util.dependency.plugin;
 
+import lombok.SneakyThrows;
 import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPluginLoader;
 
 import java.io.File;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.Comparator;
+import java.util.stream.Collectors;
 
 public class DependencyPluginTest extends DependencyPlugin {
     public DependencyPluginTest() {
@@ -19,9 +24,17 @@ public class DependencyPluginTest extends DependencyPlugin {
         //not used
     }
 
+    @SneakyThrows
     @Override
     protected void disable() {
-        //not used
+        for (Path path : Files.walk(this.getPluginFolder().resolve("log"))
+                .sorted(Comparator.reverseOrder())
+                .collect(Collectors.toList())) {
+
+            Files.delete(path);
+        }
+
+        Files.deleteIfExists(this.getPluginFolder().resolve("log"));
     }
 
     @Override
