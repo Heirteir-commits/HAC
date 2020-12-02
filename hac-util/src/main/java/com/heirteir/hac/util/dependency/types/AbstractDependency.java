3863e297e3e7935d3db7c666c0169b0cb8668e45
heretere <heirteir@protonmail.com>
2020-12-02 05:07:34 -0500

    - Update Dependency Management System   -- Added auto updater to HAC.Core   -- Added auto downloader to HAC.Movement that downloads HAC.Core

--- hac-util/src/main/java/com/heirteir/hac/util/dependency/types/AbstractDependency.java
+++ hac-util/src/main/java/com/heirteir/hac/util/dependency/types/AbstractDependency.java
@@ -3,14 +3,25 @@ package com.heirteir.hac.util.dependency.types;
 import lombok.AccessLevel;
 import lombok.RequiredArgsConstructor;
 
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.nio.file.Path;
 
 @RequiredArgsConstructor(access = AccessLevel.PROTECTED)
 public abstract class AbstractDependency {
+    public abstract boolean needsUpdate();
 
-    public abstract URL getUrl();
+    public abstract Path getDownloadLocation();
+
+    public abstract URL getManualURL() throws MalformedURLException;
+
+    public abstract URL getUrl() throws MalformedURLException;
 
     public abstract String getName();
+
+    public abstract boolean download();
+
+    public abstract boolean load();
 
     @Override
     public abstract String toString();
