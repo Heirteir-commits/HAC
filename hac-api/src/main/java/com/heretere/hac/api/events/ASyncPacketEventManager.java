56cb9b5fa3b574aabbee0678ebbc96db00c3db79
heretere <heirteir@protonmail.com>
2020-12-09 09:20:36 -0500

    Recoding event manager

--- hac-api/src/main/java/com/heretere/hac/api/events/ASyncPacketEventManager.java
+++ hac-api/src/main/java/com/heretere/hac/api/events/ASyncPacketEventManager.java
@@ -1,129 +1,37 @@
 package com.heretere.hac.api.events;
 
-import com.heretere.hac.api.HACAPI;
-import com.heretere.hac.api.events.types.AbstractEventExecutor;
-import com.heretere.hac.api.events.types.Event;
-import com.heretere.hac.api.events.types.packets.AbstractPacketEventExecutor;
-import com.heretere.hac.api.events.types.packets.wrapper.WrappedPacket;
+import com.google.common.collect.Maps;
+import com.heretere.hac.api.events.packets.wrapper.WrappedPacket;
 import com.heretere.hac.api.player.HACPlayer;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
-import java.util.ArrayList;
-import java.util.Comparator;
-import java.util.List;
+import java.util.Map;
 import java.util.function.BiConsumer;
 
-/**
- * The type A sync packet event manager.
- */
-public final class ASyncPacketEventManager {
-    private final List<AbstractPacketEventExecutor<?>> packetEvents;
-    private final List<AbstractEventExecutor<?>> events;
+public class ASyncPacketEventManager {
+    private final Map<Class<? extends WrappedPacket>, PacketEventHandler> packetExecutors;
 
-    /**
-     * Instantiates a new A sync packet event manager.
-     */
     public ASyncPacketEventManager() {
-        this.packetEvents = new ArrayList<AbstractPacketEventExecutor<?>>() {
-            @Override
-            public boolean add(AbstractPacketEventExecutor<?> abstractPacketEventExecutor) {
-                boolean output = super.add(abstractPacketEventExecutor);
-                this.sort(Comparator.comparingInt(event -> event.getPriority().ordinal()));
-                return output;
-            }
-        };
-
-        this.events = new ArrayList<AbstractEventExecutor<?>>() {
-            @Override
-            public boolean add(AbstractEventExecutor<?> abstractEventExecutor) {
-                boolean output = super.add(abstractEventExecutor);
-                this.sort(Comparator.comparingInt(event -> event.getPriority().ordinal()));
-                return output;
-            }
-        };
+        this.packetExecutors = Maps.newHashMap();
     }
 
-    /**
-     * Add packet event.
-     *
-     * @param event the event
-     */
-    public void addPacketEvent(@NotNull AbstractPacketEventExecutor<?> event) {
-        this.packetEvents.add(event);
+    public <T extends WrappedPacket> void registerPacketEventExecutor(AbstractPacketEventExecutor<T> executor) {
+        this.getPacketEventHandler(executor.getWrappedClass()).addExecutor(executor);
     }
 
-    /**
-     * Remove packet event.
-     *
-     * @param event the event
-     */
-    public void removePacketEvent(@NotNull AbstractPacketEventExecutor<?> event) {
-        this.packetEvents.remove(event);
+    public <T extends WrappedPacket> void unregisterPacketEventExecutor(AbstractPacketEventExecutor<T> executor) {
+        this.getPacketEventHandler(executor.getWrappedClass()).removeExecutor(executor);
     }
 
-    /**
-     * Add event.
-     *
-     * @param event the event
-     */
-    public void addEvent(@NotNull AbstractEventExecutor<?> event) {
-        this.events.add(event);
+    private PacketEventHandler getPacketEventHandler(Class<? extends WrappedPacket> packetClass) {
+        return this.packetExecutors.computeIfAbsent(packetClass, pc -> new PacketEventHandler());
     }
 
-    /**
-     * Remove event.
-     *
-     * @param event the event
-     */
-    public void removeEvent(@NotNull AbstractEventExecutor<?> event) {
-        this.events.remove(event);
-    }
-
-    /**
-     * Call packet event.
-     *
-     * @param player the player
-     * @param packet the packet
-     */
-    public void callPacketEvent(@NotNull HACPlayer player, @NotNull WrappedPacket packet) {
-        this.callPacketEvent(player, packet, null);
-    }
-
-    /**
-     * Call packet event.
-     *
-     * @param player       the player
-     * @param packet       the packet
-     * @param errorHandler the error handler
-     */
     public void callPacketEvent(@NotNull HACPlayer player, @NotNull WrappedPacket packet, @Nullable BiConsumer<? super Void, ? super Throwable> errorHandler) {
-        player.runTaskASync(() -> this.packetEvents.stream()
-                .filter(e -> e.getWrappedClass().equals(packet.getClass()))
-                .filter(e -> !e.update(player, packet))
-                .findFirst()
-                .ifPresent(e -> e.onStop(player, packet)), errorHandler);
-    }
-
-    /**
-     * Call event.
-     *
-     * @param event the event
-     */
-    public void callEvent(@NotNull Event event) {
-        HACPlayer player = HACAPI.getInstance().getHacPlayerList().getPlayer(event.getPlayer().getUniqueId());
-
-        player.runTaskASync(() -> this.events.stream()
-                .filter(e -> e.getEventClass().equals(event.getClass()))
-                .forEach(e -> e.run(player, event)), null);
-    }
-
-    /**
-     * Gets current packet events.
-     *
-     * @return the current packet events
-     */
-    public List<AbstractPacketEventExecutor<?>> getCurrentPacketEvents() {
-        return this.packetEvents;
+        player.runTaskASync(
+                () -> this.getPacketEventHandler(packet.getClass()).execute(player, packet)
+                , errorHandler
+        );
     }
 }
--- hac-core/all-versions/1_11_R1/src/main/java/com/heretere/hac/core/proxy/versions/eleven/packets/builder/clientside/FlyingPacketBuilder.java
+++ hac-core/all-versions/1_11_R1/src/main/java/com/heretere/hac/core/proxy/versions/eleven/packets/builder/clientside/FlyingPacketBuilder.java
@@ -1,14 +1,14 @@
 package com.heretere.hac.core.proxy.versions.eleven.packets.builder.clientside;
 
-import com.heretere.hac.api.events.types.packets.builder.PacketBuilder;
-import com.heretere.hac.api.events.types.packets.wrapper.clientside.FlyingPacket;
+import com.heretere.hac.api.events.packets.builder.AbstractPacketBuilder;
+import com.heretere.hac.api.events.packets.wrapper.clientside.FlyingPacket;
 import com.heretere.hac.api.player.HACPlayer;
 import com.heretere.hac.core.proxy.player.data.player.PlayerData;
-import mikera.vectorz.Vector2;
-import mikera.vectorz.Vector3;
+import com.heretere.hac.core.util.vector.MutableVector2F;
+import com.heretere.hac.core.util.vector.MutableVector3F;
 import net.minecraft.server.v1_11_R1.PacketPlayInFlying;
 
-public final class FlyingPacketBuilder extends PacketBuilder<FlyingPacket> {
+public final class FlyingPacketBuilder extends AbstractPacketBuilder<FlyingPacket> {
     public FlyingPacketBuilder() {
         super(
                 PacketPlayInFlying.class,
@@ -23,14 +23,14 @@ public final class FlyingPacketBuilder extends PacketBuilder<FlyingPacket> {
         PacketPlayInFlying flying = (PacketPlayInFlying) packet;
         PlayerData playerData = player.getDataManager().getData(PlayerData.class);
 
-        Vector3 location = playerData.getCurrent().getLocation();
+        MutableVector3F location = playerData.getCurrent().getLocation();
         double x = flying.a(location.getX());
         double y = flying.b(location.getY());
         double z = flying.c(location.getZ());
 
-        Vector2 direction = playerData.getCurrent().getDirection();
-        double yaw = flying.a((float) direction.getX());
-        double pitch = flying.a((float) direction.getY());
+        MutableVector2F direction = playerData.getCurrent().getDirection();
+        double yaw = flying.a(direction.getX());
+        double pitch = flying.a(direction.getY());
 
         return new FlyingPacket(
                 x,
