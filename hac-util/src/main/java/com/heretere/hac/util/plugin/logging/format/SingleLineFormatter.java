7a96b1f5b3c1ff33bbb030c12861ac45f7ec4f8d
Imagine <70789387+imaginedevMC@users.noreply.github.com>
2020-12-22 22:54:22 -0500

    Update SingleLineFormatter.java

--- hac-util/src/main/java/com/heretere/hac/util/plugin/logging/format/SingleLineFormatter.java
+++ hac-util/src/main/java/com/heretere/hac/util/plugin/logging/format/SingleLineFormatter.java
@@ -15,6 +15,11 @@ public final class SingleLineFormatter extends Formatter {
      */
     private static final Pattern STRIP_COLOR = Pattern.compile("\u001b\\[[^m]*+");
 
+    /**
+     * Format LogRecord to string
+     * @param record the LogRecord
+     * @return the string formatted
+     */
     @Override
     public String format(@NotNull final LogRecord record) {
         return STRIP_COLOR.matcher("[" + record.getLevel() + "] " +
